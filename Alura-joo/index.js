fdcc8892a4b2a2171f5860035e22ec69c4895910
ViniciusFarias <fsvinicius9@gmail.com>
2022-03-15 16:02:00 -0300

    index js

--- Alura-joo/index.js
+++ Alura-joo/index.js
@@ -1,16 +1,28 @@
-const cliente1Nome = "Ricardo";
-const cliente1CPF = 11122233309;
-const cliente1Agencia = 1001;
-const cliente1Saldo = 0;
+class Cliente {
+    nome;
+    cpf;
+    agencia;
+    saldo;
+    rg;
+}
 
-const cliente2Nome = "Vinicius";
-const cliente2CPF = 22233344408;
-const cliente2Agencia = 1001;
-const cliente2Saldo = 0;
+const cliente1 = new Cliente();
+const cliente2 = new Cliente();
 
-const cliente3Nome = "Alice";
-const cliente3CPF = 8882223309;
-const cliente3Agencia = 1001;
-const cliente3Saldo = 0;
+cliente1.nome = "Ricardo";
+cliente1.cpf = 11122233309;
+cliente1.agencia = 1001;
+cliente1.saldo = 0;
+cliente1.rg = 123123132;
 
-console.log(cliente1Nome)
+cliente2.nome = "Vinicius";
+cliente2.cpf = 22233344408;
+cliente2.agencia = 1001;
+cliente2.saldo = 0;
+//  cliente3.nome = "Alice";
+//  cliente3.cpf = 8882223309;
+//  cliente3.agencia = 1001;
+//  cliente3.saldo = 0;
+
+console.log(cliente1)
+console.log(cliente2)
