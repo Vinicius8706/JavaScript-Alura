333ed198f7221271c3253f78e0c5105c740eea5e
ViniciusFarias <fsvinicius9@gmail.com>
2022-03-16 10:42:20 -0300

    class _saldo and depositar

--- Alura-joo/index.js
+++ Alura-joo/index.js
@@ -4,18 +4,18 @@ class Cliente {
 }
 class ContaCorrente {
     agencia;
-    saldo;
+    _saldo = 0;
 
     sacar(valor) {
-        if (this.saldo >= valor) {
-            this.saldo -= valor; //consegue encapsular dentro de um metodo
+        if (this._saldo >= valor) {
+            this._saldo -= valor; //consegue encapsular dentro de um metodo
 
         }
     }
 
     depositar(valor) {
         if (valor > 0) {
-            this.saldo += valor;
+            this._saldo += valor;
         }
 
     }
@@ -29,21 +29,11 @@ const cliente2 = new Cliente();
 cliente2.nome = "Vinicius";
 cliente2.cpf = 22233344408;
 
-//  cliente3.nome = "Alice";
-//  cliente3.cpf = 8882223309;
-//  cliente3.agencia = 1001;
-//  cliente3.saldo = 0;
 const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.saldo = 0
 contaCorrenteRicardo.agencia = 1001
 
-console.log(contaCorrenteRicardo.saldo)
 contaCorrenteRicardo.depositar(100);
 contaCorrenteRicardo.depositar(200);
 contaCorrenteRicardo.depositar(-1);
-console.log(contaCorrenteRicardo.saldo)
 contaCorrenteRicardo.sacar(50)
-console.log(contaCorrenteRicardo.saldo);
-console.log(cliente1)
-console.log(cliente2)
-console.log(cliente2)
+console.log(contaCorrenteRicardo);
